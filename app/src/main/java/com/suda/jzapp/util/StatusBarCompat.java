5645416237760b6f279dfe7835dd064f55b095ac
guhb <guhb@66d557fd-6b6b-4fd8-9e71-30fe2dca442d>
2016-04-08 13:39:20 +0000

    暂时放弃4.4以下适配
    git-svn-id: https://laptop-m8qnierp/svn/JzApp@40 66d557fd-6b6b-4fd8-9e71-30fe2dca442d

--- app/src/main/java/com/suda/jzapp/util/StatusBarCompat.java
+++ app/src/main/java/com/suda/jzapp/util/StatusBarCompat.java
@@ -25,24 +25,24 @@ public class StatusBarCompat {
             return;
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            int color = COLOR_DEFAULT;
-            ViewGroup contentView = (ViewGroup) activity.findViewById(android.R.id.content);
-            if (statusColor != INVALID_VAL) {
-                color = statusColor;
-            }
-            View statusBarView = contentView.getChildAt(0);
-            //改变颜色时避免重复添加statusBarView
-            if (statusBarView != null && statusBarView.getMeasuredHeight() == getStatusBarHeight(activity)) {
-                statusBarView.setBackgroundColor(color);
-                return;
-            }
-            statusBarView = new View(activity);
-            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    getStatusBarHeight(activity));
-            statusBarView.setBackgroundColor(color);
-            contentView.addView(statusBarView, lp);
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+//            int color = COLOR_DEFAULT;
+//            ViewGroup contentView = (ViewGroup) activity.findViewById(android.R.id.content);
+//            if (statusColor != INVALID_VAL) {
+//                color = statusColor;
+//            }
+//            View statusBarView = contentView.getChildAt(0);
+//            //改变颜色时避免重复添加statusBarView
+//            if (statusBarView != null && statusBarView.getMeasuredHeight() == getStatusBarHeight(activity)) {
+//                statusBarView.setBackgroundColor(color);
+//                return;
+//            }
+//            statusBarView = new View(activity);
+//            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+//                    getStatusBarHeight(activity));
+//            statusBarView.setBackgroundColor(color);
+//            contentView.addView(statusBarView, lp);
+//        }
 
     }
 
