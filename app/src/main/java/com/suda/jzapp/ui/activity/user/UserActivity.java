4e0e711ffba29c0ef43922325b79c966db22b5a6
guhb <guhb@66d557fd-6b6b-4fd8-9e71-30fe2dca442d>
2016-04-13 05:59:20 +0000

    调整用户界面
    git-svn-id: https://laptop-m8qnierp/svn/JzApp@60 66d557fd-6b6b-4fd8-9e71-30fe2dca442d

--- app/src/main/java/com/suda/jzapp/ui/activity/user/UserActivity.java
+++ app/src/main/java/com/suda/jzapp/ui/activity/user/UserActivity.java
@@ -1,17 +1,36 @@
 package com.suda.jzapp.ui.activity.user;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.suda.jzapp.BaseActivity;
 import com.suda.jzapp.R;
+import com.suda.jzapp.dao.greendao.User;
 import com.suda.jzapp.manager.UserManager;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserActivity extends BaseActivity {
 
     @Override
     protected void initWidget() {
-
+        mHeadIcon = (CircleImageView) findViewById(R.id.profile_image);
+        mTvUserName = (TextView) findViewById(R.id.userid);
+        userManager.getMe(new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                User user = (User) msg.obj;
+                Glide.with(UserActivity.this).
+                        load(user.getHeadImage())
+                        .placeholder(R.mipmap.suda).into(mHeadIcon);
+                mTvUserName.setText(user.getUserName());
+            }
+        });
     }
 
     @Override
@@ -19,14 +38,25 @@ public class UserActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setElevation(0);
         userManager = new UserManager(this);
+        initWidget();
     }
 
     public void logOut(View view) {
         userManager.logOut();
-        setResult(RESULT_OK );
+        setResult(RESULT_OK);
         this.finish();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        findViewById(R.id.background).setBackgroundColor(mainColor);
+    }
+
     private UserManager userManager;
+    private CircleImageView mHeadIcon;
+    private TextView mTvUserName;
+
 }
