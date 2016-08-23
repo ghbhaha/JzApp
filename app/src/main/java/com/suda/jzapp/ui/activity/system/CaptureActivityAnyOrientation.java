aad2989af0c85ee44b4adbfa40ea3c44827d3a11
ghbhaha <ghbhaha@gmail.com>
2016-08-23 16:49:18 +0800

    暂时移除zxing模块

--- app/src/main/java/com/suda/jzapp/ui/activity/system/CaptureActivityAnyOrientation.java
+++ app/src/main/java/com/suda/jzapp/ui/activity/system/CaptureActivityAnyOrientation.java
@@ -1,10 +1,10 @@
 package com.suda.jzapp.ui.activity.system;
 
-import com.journeyapps.barcodescanner.CaptureActivity;
+//import com.journeyapps.barcodescanner.CaptureActivity;
 
 /**
  * Created by ghbha on 2016/4/14.
  */
-public class CaptureActivityAnyOrientation extends CaptureActivity {
-
+public class CaptureActivityAnyOrientation {
+//    public class CaptureActivityAnyOrientation extends CaptureActivity {
 }
--- app/src/main/java/com/suda/jzapp/util/QRCodeUtil.java
+++ app/src/main/java/com/suda/jzapp/util/QRCodeUtil.java
@@ -2,50 +2,42 @@ package com.suda.jzapp.util;
 
 import android.graphics.Bitmap;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-
-import java.util.Hashtable;
-
 /**
  * Created by ghbha on 2016/4/13.
  */
 public class QRCodeUtil {
 
-    public static Bitmap createQRImage(String url,int QR_WIDTH,int QR_HEIGHT) {
-        try {
-            //判断URL合法性
-            if (url == null || "".equals(url) || url.length() < 1) {
-                return null;
-            }
-            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
-            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-            //图像数据转换，使用了矩阵转换
-            BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
-            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
-            //下面这里按照二维码的算法，逐个生成二维码的图片，
-            //两个for循环是图片横列扫描的结果
-            for (int y = 0; y < QR_HEIGHT; y++) {
-                for (int x = 0; x < QR_WIDTH; x++) {
-                    if (bitMatrix.get(x, y)) {
-                        pixels[y * QR_WIDTH + x] = 0xff000000;
-                    } else {
-                        pixels[y * QR_WIDTH + x] = 0xffffffff;
-                    }
-                }
-            }
-            //生成二维码图片的格式，使用ARGB_8888
-            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
-            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
-
-            return bitmap;
-        } catch (WriterException e) {
-            e.printStackTrace();
-
-        }
+    public static Bitmap createQRImage(String url, int QR_WIDTH, int QR_HEIGHT) {
+//        try {
+//            //判断URL合法性
+//            if (url == null || "".equals(url) || url.length() < 1) {
+//                return null;
+//            }
+//            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+//            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+//            //图像数据转换，使用了矩阵转换
+//            BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
+//            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+//            //下面这里按照二维码的算法，逐个生成二维码的图片，
+//            //两个for循环是图片横列扫描的结果
+//            for (int y = 0; y < QR_HEIGHT; y++) {
+//                for (int x = 0; x < QR_WIDTH; x++) {
+//                    if (bitMatrix.get(x, y)) {
+//                        pixels[y * QR_WIDTH + x] = 0xff000000;
+//                    } else {
+//                        pixels[y * QR_WIDTH + x] = 0xffffffff;
+//                    }
+//                }
+//            }
+//            //生成二维码图片的格式，使用ARGB_8888
+//            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
+//            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+//
+//            return bitmap;
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//
+//        }
         return null;
     }
 }
