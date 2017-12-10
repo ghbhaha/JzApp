e65b5aa9658cfe5fa906ec9035ae1790643e2af4
ghbhaha <ghbhaha@gmail.com>
2017-12-10 17:34:27 +0800

    fix 退出应用后获取用户信息不全问题

--- app/src/main/java/com/suda/jzapp/dao/cloud/avos/pojo/user/MyAVUser.java
+++ app/src/main/java/com/suda/jzapp/dao/cloud/avos/pojo/user/MyAVUser.java
@@ -3,6 +3,9 @@ package com.suda.jzapp.dao.cloud.avos.pojo.user;
 import com.avos.avoscloud.AVClassName;
 import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.PaasClient;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by Suda on 2015/10/9.
@@ -10,6 +13,27 @@ import com.avos.avoscloud.AVUser;
 
 @AVClassName("MyAVUser")
 public class MyAVUser extends AVUser {
+
+
+    /**
+     * fix 退出应用后获取用户信息不全问题
+     * @return
+     */
+    public static MyAVUser getCurrentUser() {
+        try {
+            PaasClient paasClient = PaasClient.storageInstance();
+            Field currentUser = PaasClient.class.getDeclaredField("currentUser");
+            currentUser.setAccessible(true);
+            AVUser avUser = (AVUser) currentUser.get(paasClient);
+            if (avUser != null
+                    && avUser.getSessionToken() == null) {
+                currentUser.set(paasClient, null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return getCurrentUser(MyAVUser.class);
+    }
 
     public static String getCurrentUserId() {
         if (getCurrentUser() == null) {
